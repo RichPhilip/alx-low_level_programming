c45e6a8a5c48c3b01a160e2550668e14f0404c47
Philip <333kingphil@gmail.com>
2023-03-20 12:32:10 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Size of 6-size a char: %i byte(s)\n", sizeof(char));
+	printf("Size of 6-size an int: %i byte(s)\n", sizeof(int));
+	printf("Size of 6-size a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of 6-siz a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of 6-size a float: %i byte(s)\n", sizeof(float));
+	return (0);
