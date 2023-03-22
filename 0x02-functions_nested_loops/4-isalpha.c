5a613af9579a84893fbf9863d64615aee8173ced
Philip <333kingphil@gmail.com>
2023-03-21 22:20:22 -0700

    islower

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _isalpha_checks for alphabet
+ * _islower - _isalpha_checks for alphabet
  * @c: alpha to be be checked
  * Retrun: Returns 1 if c is a letter, lowercase or uppercas
  */
@@ -10,7 +10,6 @@ int _isalpha(int c)
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
 	return (1);
-	
 	}
 	return (0);
 }
