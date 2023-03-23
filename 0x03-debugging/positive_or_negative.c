4d9fc7a9c4bf45abb657639d79d112da9943f6e3
Philip <333kingphil@gmail.com>
2023-03-23 05:05:22 -0700

    tasting

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -8,12 +8,8 @@
 
 void positive_or_negative(int n)
 {
-	if (i > 0)
-	{
-		printf("%d is positive\n", i);
-	}
-	else
-	{
+	if (i < 0)
 		printf("%d is negative\n", i);
-	}
+	else
+		printf("%d is positive\n", i);
 }
